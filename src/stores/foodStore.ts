9f25d2442932f01efa876719c15454fa29522246
JiHyoung You <fbwlgud51@gmail.com>
2026-01-27 23:03:38 +0900

    feat: 선택된 음식 로컬 스토리지에 저장

--- src/stores/foodStore.ts
+++ src/stores/foodStore.ts
@@ -1,11 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface FoodStoreProps {
   selectedFood: string;
   setSelectedFood: (food: string) => void;
 }
 
-export const useFoodStore = create<FoodStoreProps>((set) => ({
-  selectedFood: "토마토 스파게티",
-  setSelectedFood: (food) => set({ selectedFood: food }),
-}));
+export const useFoodStore = create<FoodStoreProps>()(
+  persist(
+    (set) => ({
+      selectedFood: "토마토 스파게티",
+      setSelectedFood: (food) => set({ selectedFood: food }),
+    }),
+    {
+      name: "food-storage", // localStorage 키 이름
+    },
+  ),
+);

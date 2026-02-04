2a6d32e91772acd2867ccc05fae433cd48da0348
JiHyoung You <fbwlgud51@gmail.com>
2026-02-04 09:02:02 +0900

    chore: 플레이리스트 업데이트

--- src/constants/playlistData.ts
+++ src/constants/playlistData.ts
@@ -1,5 +1,12 @@
 export const PLAYLIST = [
+  {
+    id: "7UK-64uhfII",
+    title: "World is mine",
+    artist: "하츠네 미쿠(初音ミク)",
+  },
+  { id: "htpM--gDJEw", title: "PODIUM", artist: "NMIXX(엔믹스)" },
   { id: "ZO0brUR1L4Q", title: "파도혁명(Wa;volution)", artist: "음율(UmYull)" },
+  { id: "JZGJLBTaFF4", title: "Crazy Over You", artist: "BLACKPINK" },
   { id: "vjGIY_GyAz4", title: "TAP", artist: "태용" },
   { id: "JyoltvsJ9Fw", title: "자처", artist: "한로로" },
 ];
